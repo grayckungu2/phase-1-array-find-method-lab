0db94c5d44207893cb4aab6ffd25bfbdb0f93c18
Grace kungu <grayckungu2@gmail.com>
2023-06-19 21:39:43 +0300

    Add changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningYear = array.find(obj => obj.result === "W");
+    return winningYear? winningYear.year : undefined;
+  }
+
+  
